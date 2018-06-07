55b973ec3115705f0a362477c02c62a6cadcc158
Allora Sisnroy <allsis@icloud.com>
2018-06-07 19:07:58 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -3,14 +3,14 @@
 def least_coins(x)
 if x - 25 >= 0 
 puts remainder = x % 25
+elsif x % 25 == 0
+  puts x / 25
 elsif remainder - 10 >= 0 
 puts remainder1 = remainder % 10 
 elsif remainder - 5 >= 0 
 puts remainder1 = remainder % 5
-elsif remainder - 1 >= 0 
+else remainder - 1 >= 0 
 puts remainder1 = remainder % 1
-else x % 25 == 0
-  puts x % 25
 end 
 end
 least_coins(50)
