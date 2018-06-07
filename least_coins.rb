fc3f9affffa5e9fb1fc7e3313837a30fd68cce6d
Allora Sisnroy <allsis@icloud.com>
2018-06-07 19:22:32 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -4,14 +4,17 @@ if x % 25 == 0
 puts x / 25
 elsif x - 25 >= 0 
 puts remainder = x % 25
+end
 elsif remainder % 10 == 0
 puts remainder / 10
 elsif remainder - 10 >= 0 
 puts remainder1 = remainder % 10 
+end
 elsif remainder % 5 == 0
 puts remainder / 5
 elsif remainder - 5 >= 0 
 puts remainder2 = remainder1 % 5
+end
 elsif remainder % 1 == 0
 puts remainder / 1
 else remainder - 1 >= 0 
