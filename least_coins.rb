36a89597b8c172b099ffb1336d4fc9203cb295f6
Allora Sisnroy <allsis@icloud.com>
2018-06-07 19:02:21 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,10 @@
 #write out your code here
 
-def least_coins(cents)
-
-#Code your answer here!
-
+def least_coins(x)
+if x - 25 >= 0 
+remainder = x % 25
+elsif remainder - 10 >= 0 
+puts remainder1 = remainder % 10 
+end 
 end
+least_coins(43)
