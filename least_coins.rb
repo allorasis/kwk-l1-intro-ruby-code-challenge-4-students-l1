17ecb4999d91750880d90dc19a0cb1bae55ff7ba
Allora Sisnroy <allsis@icloud.com>
2018-06-07 19:32:13 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,33 +1,25 @@
 
-def quarters(x)
+def least_coins(x)
 if x % 25 == 0
 puts x / 25
-else x - 25 >= 0 
+elsif x - 25 >= 0 
 puts remainder = x % 25
-end
-end
-def dimes(x)
-if remainder % 10 == 0
+elsif remainder % 10 == 0
 puts remainder / 10
-else remainder - 10 >= 0 
+elsif remainder - 10 >= 0 
 puts remainder1 = remainder % 10 
-end
-end
-def nickles(x)
-if remainder % 5 == 0
+elsif remainder % 5 == 0
 puts remainder / 5
-else remainder - 5 >= 0 
+elsif remainder - 5 >= 0 
 puts remainder2 = remainder1 % 5
-end
-end
-def pennies(x)
-if remainder % 1 == 0
+elsif remainder % 1 == 0
 puts remainder / 1
 else remainder - 1 >= 0 
 puts remainder3 = remainder2 % 1
 end 
 end
-quarters(55)
-dimes(55)
-nickles(55)
-pennies(55)
+least_coins(55)
+quarter = 25
+dime = 10
+nickel = 5
+penny = 1 
